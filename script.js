be409f7fc59bdf210b4d04ffd0b678c289ee5d35
C.O Sistemas <caiosan321@icloud.com>
2026-02-08 14:05:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function openWhatsApp() {
+  // Eventos de conversão
   fbq('track', 'Contact');
   fbq('track', 'Lead');
 
-  const text = encodeURIComponent(
+  const mensagem = encodeURIComponent(
     "Olá, gostaria de conversar sobre um projeto de sistema sob medida."
   );
 
   window.open(
-    "https://wa.me/5511963266825?text=" + text,
+    "https://wa.me/5511963266825?text=" + mensagem,
     "_blank"
   );
 }
